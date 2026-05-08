1ae1557c301dfdb14bc8663f09b2581190336d48
pastillazz <robertoalonsopelayo26@gmail.com>
2026-05-08 14:28:30 -0400

    v-0.1

---
+++ src/main/java/com/pastillazz/f1nt3ch/F1nt3chApplication.java
@@ -0,0 +1,13 @@
+package com.pastillazz.f1nt3ch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1nt3chApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F1nt3chApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pastillazz/f1nt3ch/F1nt3chApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pastillazz.f1nt3ch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1nt3chApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
